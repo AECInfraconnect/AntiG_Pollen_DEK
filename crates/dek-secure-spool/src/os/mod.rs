dcad74ac9cada1e959cb40eb32d20be0a54e0b60
Grit Suwa <gritsuwa@gmail.com>
2026-06-19 20:17:40 +0700

    fix: accept tests push route and macos keystore

--- crates/dek-secure-spool/src/os/mod.rs
+++ crates/dek-secure-spool/src/os/mod.rs
@@ -1,17 +1,11 @@
 #[cfg(windows)]
 pub mod windows_dpapi;
 
-#[cfg(target_os = "macos")]
-pub mod macos_keychain;
-
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "linux", target_os = "macos"))]
 pub mod linux_keyring;
 
 #[cfg(windows)]
 pub use windows_dpapi::WindowsDpapiStore as DefaultOsKeyStore;
 
-#[cfg(target_os = "macos")]
-pub use macos_keychain::MacOsKeychainStore as DefaultOsKeyStore;
-
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "linux", target_os = "macos"))]
 pub use linux_keyring::LinuxFileFallbackStore as DefaultOsKeyStore;
