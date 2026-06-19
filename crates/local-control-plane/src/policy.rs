04c7c520dfcbaf4382e3d9ee2b5c6f65e81fd565
Grit Suwa <gritsuwa@gmail.com>
2026-06-19 20:01:22 +0700

    chore: rust fmt

--- crates/local-control-plane/src/policy.rs
+++ crates/local-control-plane/src/policy.rs
@@ -475,7 +475,12 @@ async fn simulate_policy(
             });
             let _ = state
                 .telemetry_store
-                .put_telemetry("local", "decision", ev["event_id"].as_str().unwrap_or("unknown"), &ev)
+                .put_telemetry(
+                    "local",
+                    "decision",
+                    ev["event_id"].as_str().unwrap_or("unknown"),
+                    &ev,
+                )
                 .await;
 
             Ok((
@@ -512,7 +517,12 @@ async fn simulate_policy(
             });
             let _ = state
                 .telemetry_store
-                .put_telemetry("local", "decision", ev["event_id"].as_str().unwrap_or("unknown"), &ev)
+                .put_telemetry(
+                    "local",
+                    "decision",
+                    ev["event_id"].as_str().unwrap_or("unknown"),
+                    &ev,
+                )
                 .await;
 
             Ok((
