0c7eda4c63ad664ca32a093c35d8919a0256f967
Grit Suwa <gritsuwa@gmail.com>
2026-06-22 01:43:14 +0700

    test: fix fuzz target for PolicyPresetV2 model

--- fuzz/fuzz_targets/fuzz_policy_preset_render.rs
+++ fuzz/fuzz_targets/fuzz_policy_preset_render.rs
@@ -1,43 +1,30 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use dek_policy_presets::model::{PresetApplyRequest, PolicyPreset, PresetCategory, PresetLanguage, ControlLevel, PresetTemplate};
+use dek_policy_presets::model::{DeployPresetRequest, ControlMode};
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
         if let Ok(json) = serde_json::from_str::<serde_json::Value>(s) {
-            let preset = PolicyPreset {
-                preset_id: "fuzz".into(),
-                version: "1".into(),
-                display_name: "fuzz".into(),
-                description: "fuzz".into(),
-                category: PresetCategory::ShadowAi,
-                language: PresetLanguage::Rego,
-                recommended_pep_types: vec![],
-                supported_control_levels: vec![],
-                default_control_level: ControlLevel::ObserveOnly,
-                risk_tags: vec![],
-                owasp_tags: vec![],
-                parameters: vec![],
-                template: PresetTemplate {
-                    source: "{}".into(),
-                    entrypoint: None,
-                },
-                test_cases: vec![],
-            };
-            
-            let req = PresetApplyRequest {
-                targets: Default::default(),
-                control_level: ControlLevel::ObserveOnly,
-                params: std::collections::HashMap::new(),
-            };
-            
-            // Fuzz passing malicious strings as param values
-            if let Some(obj) = json.as_object() {
-                let mut req = req;
-                for (k, v) in obj {
-                    req.params.insert(k.clone(), v.clone());
+            // Get a valid built-in preset to fuzz against
+            if let Some(preset) = dek_policy_presets::catalog::builtin_presets().into_iter().next() {
+                let req = DeployPresetRequest {
+                    preset_id: preset.id.clone(),
+                    preset_version: None,
+                    control_mode: ControlMode::Observe,
+                    selected_pep_types: vec![],
+                    targets: Default::default(),
+                    params: std::collections::BTreeMap::new(),
+                    dry_run_first: false,
+                };
+                
+                // Fuzz passing malicious strings as param values
+                if let Some(obj) = json.as_object() {
+                    let mut req = req;
+                    for (k, v) in obj {
+                        req.params.insert(k.clone(), v.clone());
+                    }
+                    let _ = dek_policy_presets::render::render(&preset, &req);
                 }
-                let _ = dek_policy_presets::render(&preset, &req);
             }
         }
     }
